6987c2a36b3dae7ba6c513ca595d5d9d0f50dd7e
zsoobratty <49350914+zsoobratty@users.noreply.github.com>
2020-05-12 18:47:23 +0100

    add ui for home page

--- src/components/screens/Home.js
+++ src/components/screens/Home.js
@@ -1,10 +1,24 @@
-import React from 'react'
+import React from "react";
 
 const Home = () => {
-   return(
-       <h1>Home</h1>
-   )
-}
+  return (
+    <div className="home">
+      <div className="card home-card">
+        <h5>Dude 1</h5>
+        <div className="card-image">
+          <img src="https://images.unsplash.com/photo-1498550744921-75f79806b8a7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80" />
+        </div>
+        <div className="card-content">
+          <i class="material-icons" style={{ color: "red" }}>
+            favorite
+          </i>
+          <h6>Title</h6>
+          <p>This is the post</p>
+          <input type="text" placeholder="Add a comment" />
+        </div>
+      </div>
+    </div>
+  );
+};
 
-
-export default Home
+export default Home;
