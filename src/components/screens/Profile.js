d1304d64af7b2f79e55ac3712ea9aeeea76cc899
zsoobratty <49350914+zsoobratty@users.noreply.github.com>
2020-05-12 18:37:48 +0100

    add ui for profile

--- src/components/screens/Profile.js
+++ src/components/screens/Profile.js
@@ -1,10 +1,38 @@
-import React from 'react'
+import React from "react";
 
 const Profile = () => {
-   return(
-       <h1>Profile</h1>
-   )
-}
+  return (
+    <div style={{maxWidth:"550px",margin:"0px auto"}}>
+        <div style={{
+            display:"flex",
+            justifyContent:"space-around",
+            margin:"18px 0px",
+            borderBottom:"1px solid grey"
+          }}>
+        <div>
+          <img
+            style={{ width: "160px", height: "160px", borderRadius: "80px" }}
+            src="https://images.unsplash.com/photo-1586287011575-a23134f797f9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"
+          />
+        </div>
+        <div>
+          <h4>Some dude</h4>
+          <div style={{display:"flex", justifyContent:"space-between", width:"108%"}}>
+            <h6>40 Posts</h6>
+            <h6>40 Followers</h6>
+            <h6>40 Following</h6>
+          </div>
+        </div>
+      </div>
+        <div className="gallery">
+            <img className="item" src="https://images.unsplash.com/photo-1586287011575-a23134f797f9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"/>
+            <img className="item" src="https://images.unsplash.com/photo-1586287011575-a23134f797f9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"/>
+            <img className="item" src="https://images.unsplash.com/photo-1586287011575-a23134f797f9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"/>
+            <img className="item" src="https://images.unsplash.com/photo-1586287011575-a23134f797f9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"/>
+            <img className="item" src="https://images.unsplash.com/photo-1586287011575-a23134f797f9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"/>
+        </div>
+    </div>
+  );
+};
 
-
-export default Profile
+export default Profile;
