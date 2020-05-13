aef8825165b2250dda37b1ae1725a16c87e52dfa
zsoobratty <49350914+zsoobratty@users.noreply.github.com>
2020-05-13 18:04:32 +0100

    create post and upload image connected to db and cloudinary

--- src/components/screens/CreatePost.js
+++ src/components/screens/CreatePost.js
@@ -1,6 +1,52 @@
-import React from "react";
-
+import React, { useState } from "react";
+import M from "materialize-css";
+import {useHistory} from 'react-router-dom'
 const CreatePost = () => {
+  const history = useHistory()
+  const [title, setTitle] = useState("");
+  const [body, setBody] = useState("");
+  const [image, setImage] = useState("");
+  const [url, setUrl] = useState("");
+
+  const postDetails = () => {
+    const data = new FormData();
+    data.append("file", image);
+    data.append("upload_preset", "instaclone");
+    data.append("cloud_name", "cloudziad");
+    fetch("	https://api.cloudinary.com/v1_1/cloudziad/image/upload", {
+      method: "post",
+      body: data,
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        setUrl(data.url);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+      fetch("/createpost", {
+        method: "post",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+          title,
+          body,
+          url
+        }),
+      })
+        .then((res) => res.json())
+        .then((data) => {
+          if (data.error) {
+            M.toast({ html: data.error, classes: "#e53935 red darken-1" });
+          } else {
+            M.toast({ html: "Post created successfully", classes: "#43a047 green darken-1" });
+            history.push("/");
+          }
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+  };
+
   return (
     <div
       className="card input-field"
@@ -11,18 +57,35 @@ const CreatePost = () => {
         textAlign: "center",
       }}
     >
-      <input type="text" placeholder="title" />
-      <input type="text" placeholder="message" />
+      <input
+        type="text"
+        placeholder="title"
+        value={title}
+        onChange={(e) => {
+          setTitle(e.target.value);
+        }}
+      />
+      <input
+        type="text"
+        placeholder="message"
+        value={body}
+        onChange={(e) => {
+          setBody(e.target.value);
+        }}
+      />
       <div className="file-field input-field">
         <div className="btn #64b5f6 blue darken-1">
           <span>Upload Image</span>
-          <input type="file" />
+          <input type="file" onChange={(e) => setImage(e.target.files[0])} />
         </div>
         <div className="file-path-wrapper">
           <input className="file-path validate" type="text" />
         </div>
       </div>
-      <button className="btn waves-effect waves-light #64b5f6 blue darken-1">
+      <button
+        className="btn waves-effect waves-light #64b5f6 blue darken-1"
+        onClick={() => postDetails()}
+      >
         Submit Post
       </button>
     </div>
