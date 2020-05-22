601e1a01d452e2928d9a73ff209842b31dd90b2e
zsoobratty <49350914+zsoobratty@users.noreply.github.com>
2020-05-22 13:56:34 +0100

    implement logout and clear localstorage

--- src/components/screens/Signup.js
+++ src/components/screens/Signup.js
@@ -56,7 +56,7 @@ const SignUp = () => {
           onChange={(e) => setEmail(e.target.value)}
         />
         <input
-          type="text"
+          type="password"
           placeholder="password"
           value={password}
           onChange={(e) => setPassword(e.target.value)}
