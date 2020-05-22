8b0675a70388eb03a0b38a297504a8aeec48ee01
zsoobratty <49350914+zsoobratty@users.noreply.github.com>
2020-05-22 14:47:15 +0100

    implement like and unlike into react

--- src/components/screens/Home.js
+++ src/components/screens/Home.js
@@ -1,7 +1,9 @@
-import React,{useState, useEffect} from "react";
+import React,{useState, useEffect, useContext} from "react";
+import {UserContext} from '../../App'
 
 const Home = () => {
   const [data, setData] = useState([])
+  const {state,dispatch} = useContext(UserContext)
   useEffect(()=>{
     fetch('/allposts',{
       headers:{
@@ -9,9 +11,63 @@ const Home = () => {
       }
     }).then(res=>res.json())
     .then(result=>{
+      console.log(result)
       setData(result.posts)
     })
   },[])
+
+  const likePost = (id)=>{
+    fetch('/like',{
+      method:"put",
+      headers:{
+      "Content-Type":"application/json",
+      "Authorisation":"Bearer "+localStorage.getItem('jwt')
+      },
+      body:JSON.stringify({
+        postId:id
+      })
+    }).then(res=>res.json())
+    .then(result=>{
+      // console.log(result)
+      const newData = data.map(item=>{
+        if(item._id==result._id){
+          return result
+        } else {
+          return item
+        }
+      })
+      setData(newData)
+    }).catch(err=>{
+      console.log(err)
+    })
+  }
+
+  const unlikePost = (id)=>{
+    fetch('/unlike',{
+      method:"put",
+      headers:{
+      "Content-Type":"application/json",
+      "Authorisation":"Bearer "+localStorage.getItem('jwt')
+      },
+      body:JSON.stringify({
+        postId:id
+      })
+    }).then(res=>res.json())
+    .then(result=>{
+      // console.log(result)
+      const newData = data.map(item=>{
+        if(item._id==result._id){
+          return result
+        } else {
+          return item
+        }
+      })
+      setData(newData)
+    }).catch(err=>{
+      console.log(err)
+    })
+  }
+
   return (
     <div className="home">
       {
@@ -23,9 +79,20 @@ const Home = () => {
               <img src={item.photo} />
             </div>
             <div className="card-content">
-              <i className="material-icons" style={{ color: "red" }}>
+            <i className="material-icons" style={{ color: "red" }}>
                 favorite
               </i>
+              {item.likes.includes(state._id)
+              ? 
+                <i className="material-icons" style={{ color: "red"}} onClick={()=>{unlikePost(item._id)}}>
+                  thumb_down
+                </i>
+              :
+                <i className="material-icons" style={{ color: "green" }} onClick={()=>{likePost(item._id)}}>
+                  thumb_up
+                </i>
+              }
+              <h6>{item.likes.length} likes</h6>
               <h6>{item.title}</h6>
               <p>{item.body}</p>
               <input type="text" placeholder="Add a comment" />
