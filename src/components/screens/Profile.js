e4c6a47829a7615380f92f5e5dd60b12093e7b2b
zsoobratty <49350914+zsoobratty@users.noreply.github.com>
2020-05-27 16:35:33 +0100

    add logic for update profile picture

--- src/components/screens/Profile.js
+++ src/components/screens/Profile.js
@@ -4,6 +4,8 @@ import {UserContext} from '../../App'
 const Profile = () => {
   const [myPics,setPics] = useState([])
   const {state,dispatch} = useContext(UserContext)
+  const [image,setImage] = useState("")
+  const [url,setUrl] = useState(undefined)
   useEffect(()=>{
     fetch('/myposts',{
       headers:{
@@ -14,13 +16,42 @@ const Profile = () => {
       setPics(result.myPost)
     })
   })
+
+  useEffect(()=>{
+    if(image){
+      const data = new FormData();
+    data.append("file", image);
+    data.append("upload_preset", "instaclone");
+    data.append("cloud_name", "cloudziad");
+    fetch("	https://api.cloudinary.com/v1_1/cloudziad/image/upload", {
+      method: "post",
+      body: data,
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        setUrl(data.url);
+        console.log(data)
+        localStorage.setItem("user",JSON.stringify({...state,pic:data.url}))
+        dispatch({type:"UPDATEPIC",payload:data.url})
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+    }
+  },[image])
+
+  const updatePhoto = (file)=>{
+    setImage(file)
+  }
   return (
     <div style={{maxWidth:"550px",margin:"0px auto"}}>
+      <div style={{
+            margin:"18px 0px",
+            borderBottom:"1px solid grey"
+          }}>
         <div style={{
             display:"flex",
             justifyContent:"space-around",
-            margin:"18px 0px",
-            borderBottom:"1px solid grey"
           }}>
         <div>
           <img
@@ -37,6 +68,16 @@ const Profile = () => {
             <h6>{state?state.following.length:"0"} Following</h6>
           </div>
         </div>
+      </div>
+        <div className="file-field input-field" style={{margin:"10px"}}>
+        <div className="btn #64b5f6 blue darken-1">
+          <span>Upload Profile Photo</span>
+          <input type="file" onChange={(e) => updatePhoto(e.target.files[0])} />
+        </div>
+        <div className="file-path-wrapper">
+          <input className="file-path validate" type="text" />
+        </div>
+      </div>
       </div>
         <div className="gallery">
           {
