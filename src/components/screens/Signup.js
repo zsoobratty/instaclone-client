5d919e8716800b5461e74aa05e97ee6238e50a7f
zsoobratty <49350914+zsoobratty@users.noreply.github.com>
2020-05-27 16:05:48 +0100

    add profile pictures for users

--- src/components/screens/Signup.js
+++ src/components/screens/Signup.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Link, useHistory } from "react-router-dom";
 import M from "materialize-css";
 
@@ -7,36 +7,71 @@ const SignUp = () => {
   const [name, setName] = useState("");
   const [password, setPassword] = useState("");
   const [email, setEmail] = useState("");
-  const PostData = () => {
-    if (
-      !/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(
-        email
-      )
-    ) {
-      M.toast({ html: "Invalid Email", classes: "#e53935 red darken-1" });
-      return;
+  const [image,setImage] = useState("")
+  const [url,setUrl] = useState(undefined)
+  useEffect(()=>{
+    if(url){
+      uploadFields()
     }
-    fetch("/signup", {
+  },[url])
+  
+  const uploadPhoto = ()=>{
+    const data = new FormData();
+    data.append("file", image);
+    data.append("upload_preset", "instaclone");
+    data.append("cloud_name", "cloudziad");
+    fetch("	https://api.cloudinary.com/v1_1/cloudziad/image/upload", {
       method: "post",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({
-        name,
-        password,
-        email,
-      }),
+      body: data,
     })
       .then((res) => res.json())
       .then((data) => {
-        if (data.error) {
-          M.toast({ html: data.error, classes: "#e53935 red darken-1" });
-        } else {
-          M.toast({ html: data.message, classes: "#43a047 green darken-1" });
-          history.push("/signin");
-        }
+        setUrl(data.url);
       })
       .catch((err) => {
         console.log(err);
       });
+  }
+
+const uploadFields = ()=> {
+  if (
+    !/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(
+      email
+    )
+  ) {
+    M.toast({ html: "Invalid Email", classes: "#e53935 red darken-1" });
+    return;
+  }
+  fetch("/signup", {
+    method: "post",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({
+      name,
+      password,
+      email,
+      pic:url
+    }),
+  })
+    .then((res) => res.json())
+    .then((data) => {
+      if (data.error) {
+        M.toast({ html: data.error, classes: "#e53935 red darken-1" });
+      } else {
+        M.toast({ html: data.message, classes: "#43a047 green darken-1" });
+        history.push("/signin");
+      }
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+}
+
+  const PostData = () => {
+    if(image){
+      uploadPhoto()
+    } else {
+      uploadFields()
+    }
   };
 
   return (
@@ -61,6 +96,15 @@ const SignUp = () => {
           value={password}
           onChange={(e) => setPassword(e.target.value)}
         />
+        <div className="file-field input-field">
+        <div className="btn #64b5f6 blue darken-1">
+          <span>Upload Profile Photo</span>
+          <input type="file" onChange={(e) => setImage(e.target.files[0])} />
+        </div>
+        <div className="file-path-wrapper">
+          <input className="file-path validate" type="text" />
+        </div>
+      </div>
         <button
           className="btn waves-effect waves-light #64b5f6 blue darken-1"
           onClick={() => PostData()}
